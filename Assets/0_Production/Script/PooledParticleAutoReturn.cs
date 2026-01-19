37c4fcb75225ccfd8ff8a24ede3ddda216d60bd1
이윤기 <yglee07@dopaminefactory.io>
2026-01-19 18:38:01 +0900

    풀링 화 + 인펙터 이름 설정 스테이지 미루기

---
+++ Assets/0_Production/Script/PooledParticleAutoReturn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PooledParticleAutoReturn : MonoBehaviour
+{
+    [SerializeField] string poolKey;
+    ParticleSystem ps;
+
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnEnable()
+    {
+        if (ps != null)
+            ps.Play();
+    }
+
+    void Update()
+    {
+        if (ps != null && !ps.IsAlive(true))
+        {
+            PoolManager.Instance.Despawn(poolKey, gameObject);
+        }
+    }
+}
